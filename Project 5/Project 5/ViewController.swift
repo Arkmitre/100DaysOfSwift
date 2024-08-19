d28cb34ed12811925d672244bc4dda8671e5c386
Alexander Khorkov <Arkmitre@inbox.ru>
2024-08-19 18:28:43 +0500

    Refix buggfix more efficiently

--- Project 5/Project 5/ViewController.swift
+++ Project 5/Project 5/ViewController.swift
@@ -44,14 +44,10 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        if answer.lowercased() == answer { } else {
-        showErrorMessage(title: "Used Uppercased characters", message: "Please use lowercased characters instead")
-        return
-        }
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(lowerAnswer, at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
