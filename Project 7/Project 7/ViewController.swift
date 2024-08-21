f53cd83ca3b6d0a965052642527f6dbef84a93ee
Alexander Khorkov <Arkmitre@inbox.ru>
2024-08-21 16:05:43 +0500

    Add GCD to load and parse Data

--- Project 7/Project 7/ViewController.swift
+++ Project 7/Project 7/ViewController.swift
@@ -25,13 +25,16 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        if let url = URL(string: urlString){
-            if let data = try? Data(contentsOf: url) {
-                parse(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let url = URL(string: urlString){
+                if let data = try? Data(contentsOf: url) {
+                    self.parse(json: data)
+                    return
+                }
             }
+            
+            self.showError()
         }
-        showError()
     }
     
     func parse(json: Data) {
@@ -40,7 +43,9 @@ class ViewController: UITableViewController {
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             filteredPetitions = petitions
-            tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
     
@@ -63,9 +68,11 @@ class ViewController: UITableViewController {
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
     }
     
     func showErrorFilter() {
@@ -86,7 +93,7 @@ class ViewController: UITableViewController {
             guard let filter = ac?.textFields?[0].text else { return }
             self?.filterPetitions(filter)
         }
-        let clearFilter = UIAlertAction(title: "Clear filters", style: .default) { [weak self, weak ac ] action in
+        let clearFilter = UIAlertAction(title: "Clear filters", style: .default) { [weak self] action in
             self?.clearFilter()
         }
         ac.addAction(sumbitFilter)
