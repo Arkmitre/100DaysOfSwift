6f825c799cd83a358bf7bb8d0c448af8250023be
Alexander Khorkov <Arkmitre@inbox.ru>
2024-08-23 20:38:54 +0500

    Project 10 init commit. CollectionView with possibility to add cell(image,name) from user library

---
+++ Project 10/Project 10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Alexander on 23/08/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
