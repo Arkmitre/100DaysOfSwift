4cf41233723d8e0f28d80ac1c91b9288c3c82aac
Alexander Khorkov <Arkmitre@inbox.ru>
2024-08-26 21:05:05 +0500

    Project 12. Some study with usedDefaults read/write

---
+++ Project 12a/Project 10/Person.swift
@@ -0,0 +1,29 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Alexander on 23/08/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+}
---
+++ Project 12a/Project 10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Alexander on 23/08/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
