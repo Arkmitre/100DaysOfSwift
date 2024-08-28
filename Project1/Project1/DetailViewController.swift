862b446a45523be84aa38466eba9947e32c123e0
Alexander Khorkov <Arkmitre@inbox.ru>
2024-08-28 18:13:47 +0500

    Add viewsCount to images, stored in UserDefaults

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -10,47 +10,72 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var viewsCount: UILabel!
     
     var selectedImage: String?
     var imageIndex: (Int, Int)?
+    var viewCount = [Int]()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if viewCount.isEmpty { viewCount = [Int](repeating: 0, count: imageIndex!.1) }
+        read()
+        
+        
         if let imageIndex = imageIndex {
-        title = "Picture \(imageIndex.0 + 1) of \(imageIndex.1)"
+            title = "Picture \(imageIndex.0 + 1) of \(imageIndex.1)"
+            viewsCount.text = "Views: \(viewCount[imageIndex.0])"
         }
         navigationItem.largeTitleDisplayMode = .never
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
-
-        // Do any additional setup after loading the view.
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = true
+        viewCount[imageIndex!.0] += 1
+        viewsCount.text = "Views: \(viewCount[imageIndex!.0])"
+        save()
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func read() {
+        DispatchQueue.global(qos: .userInteractive).async {
+            let defaults = UserDefaults.standard
+            if let savedViewsCount = defaults.object(forKey: "viewsCount") as? Data {
+                let jsonDecoder = JSONDecoder()
+                do {
+                    self.viewCount = try jsonDecoder.decode([Int].self, from: savedViewsCount)
+                    DispatchQueue.main.async {
+                        self.viewsCount.text = "Views: \(self.viewCount[self.imageIndex!.0])"
+                    }
+                } catch {
+                    DispatchQueue.main.async { print("Failed to load viewsCount") }
+                }
+                
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func save() {
+        DispatchQueue.global(qos: .background).async {
+            let jsonEncoder = JSONEncoder()
+            
+            if let savedData = try? jsonEncoder.encode(self.viewCount) {
+                let defaults = UserDefaults.standard
+                defaults.set(savedData, forKey: "viewsCount")
+            } else {
+                print("Failed to load viewsCount")
+            }
+        }
     }
-    */
-
 }
--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -16,27 +16,26 @@ class ViewController: UITableViewController {
         
         title = "Strom Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
-        DispatchQueue.global(qos: .background).async {
-            self.loadData()
-        }
+        loadData()
         
     }
     
     @objc func loadData() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        
-        DispatchQueue.main.async {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
             for item in items {
                 if item.hasPrefix("nssl") {
-                    self.pictures.append(item)
+                        self.pictures.append(item)
                 }
             }
             self.pictures.sort()
-            self.tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
-
     }
     
     override func tableView(_ tableView: UITableView , numberOfRowsInSection: Int) -> Int {
@@ -45,18 +44,17 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
+        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
             vc.imageIndex = (indexPath.row, pictures.count)
+
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+        
 }
 
