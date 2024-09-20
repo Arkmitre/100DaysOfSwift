33eaddca48aa8883584781bf53c0b3287e92b2cf
Alexander Khorkov <Arkmitre@inbox.ru>
2024-09-20 16:45:14 +0500

    Project 19.Some study work with Extension(safari->textfield->javaScript)

---
+++ Project 19/Extension/ActionViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Alexander on 20/09/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+    var pageTitle = ""
+    var pageURL = ""
+
+    @IBOutlet var script: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
+                }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        let keyboardValue = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (keyboardValue[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == Notification.Name.UIKeyboardWillHide {
+            script.contentInset = UIEdgeInsets.zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selecterRange = script.selectedRange
+        script.scrollRangeToVisible(selecterRange)
+    }
+}
---
+++ Project 19/Project 19/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 19
+//
+//  Created by Alexander on 20/09/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
