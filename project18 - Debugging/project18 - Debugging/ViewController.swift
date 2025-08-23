d57407b122b6a4eb615ec723c2e3dadedb73f13f
Александр <arkmitre@MacBook-Air-Aleksandr.local>
2025-08-23 16:51:44 +0500

    Some debugging study idk why its separated project

---
+++ project18 - Debugging/project18 - Debugging/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  project18 - Debugging
+//
+//  Created by Александр on 22.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i).")
+        }
+    }
+
+
+}
+
