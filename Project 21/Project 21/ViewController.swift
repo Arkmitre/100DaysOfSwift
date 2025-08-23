32e5c208a3dc86408010b18ef0dc5c1afbb53810
Alexander Khorkov <Arkmitre@inbox.ru>
2025-08-23 18:42:31 +0500

    Send reminders, prompts and alerts

---
+++ Project 21/Project 21/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 21
+//
+//  Created by Alexander on 21/09/2024.
+//  Copyright © 2024 Alexander Khorkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
