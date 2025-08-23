70b1b0532c5151db32fc48b22fefb972b224ccc8
Alexander Khorkov <Arkmitre@gmail.com>
2025-08-23 20:52:42 +0500

    Project24 some Strings study

---
+++ Project24/Project24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project24
+//
+//  Created by Александр on 23.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
