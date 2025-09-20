6c78eadc393c73f6e5975c4a18a2974ac03b06ac
Alexander Khorkov <Arkmitre@gmail.com>
2025-09-20 22:09:42 +0500

    Project38
    Study with Core Data

---
+++ Project38/Project38/Author+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Author+CoreDataClass.swift
+//  Project38
+//
+//  Created by Александр on 20.09.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Author)
+public class Author: NSManagedObject {
+
+}
---
+++ Project38/Project38/Commit+CoreDataClass 2.swift
@@ -0,0 +1,15 @@
+//
+//  Commit+CoreDataClass.swift
+//  Project38
+//
+//  Created by Александр on 20.09.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
